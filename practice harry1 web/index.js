a1e46cbead4d46737f6bc4d258b8733d09df938e
nayanshiroya <nayanshiroya55@gmail.com>
2024-08-04 12:17:24 +0530

    Update index.js

--- practice harry1 web/index.js
+++ practice harry1 web/index.js
@@ -6,7 +6,7 @@ burger = $(".burger").on("click",function(){
     $(".navbar").css("transition" ," all 0.5s ease-in-out");
     $(".nav-list , .nav-right").toggleClass("vclass");
     $(".navbar").toggleClass("hclass");
-    // $(".nav-list").toggleClass("vclass");
+    
    
 
 
